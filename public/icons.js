77ca6e36e0b708106e2e18c17a9c1e7f232ddd8e
Woramet7897 <woramet7897@email.com>
2025-09-24 18:48:23 +0700

    feat: add icons and main card component with initial imports

---
+++ public/icons.js
@@ -0,0 +1,1 @@
+import { IconName } from "react-icons/go";
---
+++ src/components/main-card.tsx
@@ -0,0 +1,4 @@
+'use client'
+import React from 'react'
+import { Card, CardHeader, CardTitle, CardDescription, CardAction } from './ui/card'
+import { Button } from './ui/button'
