952325057fc5d518dbf34722f8ddf49ce6eb31e5
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-10-03 12:36:31 +0700

    feat: add background image to header in Home component for enhanced visual appeal

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
     return (
         <div className="max-w-full overflow-hidden"> {/* ป้องกัน overflow */}
-            <div className="relative bg-yellow-200 m-4 h-[128px] p-4 rounded-[20px]">
+            <div className="relative bg-yellow-200 m-4 h-[128px] p-4 rounded-[20px] bg-[url('/next.svg')] bg-right bg-no-repeat">
                 <div className="text-[16px] text-orange-600">สวัสดีวันจันทร์</div>
                 <MainButton classname="absolute right-0 bottom-0 m-4" label="กิจกรรมของฉัน" />
             </div>
